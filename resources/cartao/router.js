adfa6b9ea4bc2c46853a4f697c9d5ef09bcb7cfc
bea-girao <andysil.dgt23@gmail.com>
2023-04-04 12:36:37 -0300

    Realizada alterações no doc para incluir os verbos PATCH e PUT

--- resources/cartao/router.js
+++ resources/cartao/router.js
@@ -32,9 +32,49 @@ app.post(URL_NAME, async (req, res) => {
     res.send(dados);
 });
 
+app.patch('/cartoes/:id', async (req, res) => {
+    let dados = req.body;
 
+    let jaExiste = await database.execute(`
+        SELECT * FROM tb_cartao WHERE id='${req.params.id}'
+    `);
 
+    if (undefined === jaExiste[0]) {
+        res.sendStatus(404);
+        return;
+    }
 
+    await database.execute(`
+        UPDATE tb_cartao SET
+            id="${req.body.id || jaExiste[0].id}",
+            titular="${req.body.titular || jaExiste[0].titular}",
+            numero_do_cartao="${req.body.numero_do_cartao || jaExiste[0].numero_do_cartao}",
+            validade="${req.body.validade || jaExiste[0].validade}",
+            cvv="${req.body.cvv || jaExiste[0].cvv}"
+        WHERE id='${req.params.id}'
+    `);
+
+    dados.id = req.params.id;
+
+    res.send(dados);
+});
+
+app.put('/cartoes/:id', async (req, res) => {
+    let dados = req.body;
+
+    await database.execute(`
+        UPDATE tb_cartao SET id = '${dados.id}', 
+        titular = '${dados.titular}', 
+        numero_do_cartao = '${dados.numero_do_cartao}',
+        validade = '${dados.validade}', 
+        cvv = '${dados.cvv}'
+        WHERE id = '${req.params.id}'
+    `);
+
+    dados.id = req.params.id;
+
+    res.send(dados);
+});
 
 app.delete(`${URL_NAME}/:id`, async (req, res) => {
     await database.execute(`DELETE FROM tb_cartao WHERE id='${req.params.id}'`)
